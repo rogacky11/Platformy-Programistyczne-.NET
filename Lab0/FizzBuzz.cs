fe6e4e79aeb7ad7dc83c2a1ff49c93db351902b3
microg4311 <microg4311@W04N-P317D07>
2024-02-28 17:57:46 +0100

    Add project files.

---
+++ Lab0/FizzBuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab0
+{
+    internal class FizzBuzz
+    {
+        private int _uB;
+        public FizzBuzz(int uB) { _uB = uB; }
+        
+
+        public void Display()
+        {
+            for (int i = 0; i < _uB; i++)
+            {
+                if ((i % 5 == 0) && (i % 3 == 0)) Console.WriteLine("FizzBuzz");
+                else if (i % 3 == 0) { Console.WriteLine("Fizz"); }
+                else if (i % 5 == 0) { Console.WriteLine("Buzz"); }
+                else { Console.WriteLine(i); }
+
+            }
+        }
+
+    }
+}
---
+++ Lab0/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fz1 = new FizzBuzz(20);
+            fz1.Display();
+        }
+    }
+}
